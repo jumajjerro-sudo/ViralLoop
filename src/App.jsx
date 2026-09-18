cfd612f0add7c78ea6340eab3b8f8a85456ac6fb
Juma <jumajjjerro@gmail.com>
2026-09-18 22:40:31 +0300

    initial viralLoop frontend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Turn participation into your next big thing</h1>
+
+        <p>Discover challenges. Participate. Create. Share. Grow.
+        </p>
+      </main>
+    </>  
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return (
+        <nav>
+            <div>
+                <h2>ViralLoop</h2>
+            </div>
+
+            <div>
+                <a href="#">Explore</a>
+                <a href="#">Create</a>
+                <a href="#">Login</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
